7b7826565f9727abb0a3a192e51646a359ea5d4b
fargx32mkx <54091479+fargx32mkx@users.noreply.github.com>
2020-04-21 15:58:38 -0500

    Add files via upload

---
+++ clist/node.h
@@ -0,0 +1,17 @@
+#ifndef NODE_H
+#define NODE_H
+
+typedef int DATA;
+
+typedef struct _node node;
+
+struct _node{
+    DATA data;
+    node *next;
+};
+
+node *create_node(DATA data);
+void delete_node(node *n);
+
+#endif
+
